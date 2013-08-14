9ada28d1633fd5b5b8a1fa60fae1292a5827d29b
wangzhenhao <oldjohnwang@gmail.com>
2013-08-14 18:01:29 +0800

    Update kNN.py
    kNN算法实现：classify0();file2matrix();autoNorm()

--- kNN.py
+++ kNN.py
@@ -16,13 +16,14 @@ def classify0(inX, dataSet, labels, k):
     for i in range(k):
         voteLabel = labels[sortedDistIndicies[i]]
         classCount[voteLabel] = classCount.get(voteLabel,0) + 1
-    sortedClassCount = sorted(classCount.iteritems(), key=operator.itemgetter(1), reverse=True)
+    sortedClassCount = sorted(classCount.iteritems(),
+                              key=operator.itemgetter(1), reverse=True)
     return sortedClassCount[0][0]
 
 def file2matrix(filename):
     fr = open(filename)
     numberOfLines = len(fr.readlines())         
-    returnMat = zeros((numberOfLines,3))        #(numberOfLines,3) is a shape of a matrix
+    returnMat = zeros((numberOfLines,3))  #(numberOfLines,3) is a shape of a matrix
     classLabelVector = []                       
     fr = open(filename)
     index = 0
@@ -30,6 +31,16 @@ def file2matrix(filename):
         line = line.strip()   #get rid of the \n
         listFromLine = line.split('\t')
         returnMat[index,:] = listFromLine[0:3]
-        classLabelVector.append(int(listFromLine[-1]))  #-1 represents the last element of the line
+        classLabelVector.append(int(listFromLine[-1]))
         index += 1
     return returnMat,classLabelVector
+
+def autoNorm(dataset):
+    minv = dataset.min(0)
+    maxv = dataset.max(0)
+    ranges = maxv - minv
+    m = dataset.shape[0]
+    normdata = dataset - tile(minv,(m,1)) #tile(minv,(m,n)),expand minv:([x,y,z])
+                                          #to m*n matrix in which each element is [x,y,z]
+    normdata = normdata/tile(ranges,(m,1))
+    return normdata,ranges,minv
